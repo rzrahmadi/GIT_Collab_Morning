96c6a3708294fb3cb712c59dcb266123e6f71379
rzrahmadi <rizrpage@gmail.com>
2023-05-26 11:18:06 +0700

    contentview edited

--- GIT_Collab_Morning/ContentView.swift
+++ GIT_Collab_Morning/ContentView.swift
@@ -66,35 +66,5 @@ struct ContentView: View {
         }
     }
     
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-    }
-    
-    struct ListRowView: View {
-        var body: some View {
-            NavigationLink(destination: DetailView()) {
-                HStack(alignment: .top) {
-                    Rectangle()
-                        .frame(width: 80, height: 80)
-                        .foregroundColor(.blue)
-                    
-                    VStack(alignment: .leading) {
-                        Rectangle()
-                            .frame(width: 80, height: 20)
-                        
-                        Rectangle()
-                            .frame(height: 20)
-                        
-                        Rectangle()
-                            .frame(height: 20)
-                    }
-                    .foregroundColor(.orange)
-                }
-                .padding(.vertical, 10)
-            }
-        }
-    }
     
 }
