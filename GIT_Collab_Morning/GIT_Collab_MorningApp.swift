bdb3577cc00d5b6e25daae4b3724f42d1b2930bf
Ki <rizrpage@gmail.com>
2023-05-26 10:42:19 +0700

    Initial Commit

---
+++ GIT_Collab_Morning/GIT_Collab_MorningApp.swift
@@ -0,0 +1,17 @@
+//
+//  GIT_Collab_MorningApp.swift
+//  GIT_Collab_Morning
+//
+//  Created by Rizki Rahmadi on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct GIT_Collab_MorningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
